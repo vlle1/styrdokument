b613ad4091942af0219cddc6cceec8b7c0fe1797
Peter Boström <pbos@kth.se>
2012-03-16 14:07:37 +0100

    mer expressivt att inte tillåta h1, även om den inte hade matchats innan

--- toc_gen.rb
+++ toc_gen.rb
@@ -10,7 +10,7 @@ file = ARGV.length > 0 ? File.open(ARGV[0]) : STDIN
 out = ARGV.length > 0 ? File.open(ARGV[0].sub(/\.textile$/, '') + '.toc.textile', 'w') : STDOUT
 
 file.each_line do |line|
-  if line.match /^h([1-6])\(([^)]+)\)\.\s(.*)$/
+  if line.match /^h([2-6])\(([^)]+)\)\.\s(.*)$/
     out.puts '#' * ($1.to_i - 1) + " \"#{$3}\":#{$2}"
   end
 end
